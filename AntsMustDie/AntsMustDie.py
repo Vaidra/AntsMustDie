5226e0ba1eb46ddeceaec974fb1b35eb54c80954
Exaverine <exaverine.clin@univ-reims.fr>
2017-05-26 17:06:50 +0200

    Modification de la couleur du fond

--- AntsMustDie/AntsMustDie.py
+++ AntsMustDie/AntsMustDie.py
@@ -6,7 +6,7 @@ screen = pygame.display.set_mode((640,480))
 
 pygame.display.set_caption("ShemsCo - Pygame Tutorial")
 
-Green = pygame.Color(0,255,0)
+Green = pygame.Color(255,0,255)
 
 while True:
     screen.fill(Green)
