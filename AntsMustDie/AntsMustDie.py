26fe425735d7135a9d248257d0810a09c6da0db7
RAK0S <sokar.ysuran@yahoo.fr>
2017-05-26 17:01:42 +0200

    modif Projet

---
+++ AntsMustDie/AntsMustDie.py
@@ -0,0 +1,20 @@
+import pygame, sys
+
+pygame.init()
+
+screen = pygame.display.set_mode((640,480))
+
+pygame.display.set_caption("ShemsCo - Pygame Tutorial")
+
+Green = pygame.Color(0,255,0)
+
+while True:
+    screen.fill(Green)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+
+    pygame.display.update()
