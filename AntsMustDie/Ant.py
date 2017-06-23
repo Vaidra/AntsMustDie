621d1a2969bd44ab153bf6f6c89989356fbeba8f
Vaidra <exaverine.27clin@yahoo.fr>
2017-06-23 17:39:27 +0200

    Ant : ajout des getteurs et setteurs

--- AntsMustDie/Ant.py
+++ AntsMustDie/Ant.py
@@ -3,15 +3,26 @@ class Ant(object):
 
     def __init__ (
         self, 
+        _x_position,
+        _y_position,
         _damage     = 5, 
         _fire_speed = 1,
         _life       = 100,
         _move_speed = 1
     ):
+        self.x_position = _x_position
+        self.y_position = _y_position
         self.damage     = _damage
         self.fire_speed = _fire_speed
         self.life       = _life
         self.move_speed = _move_speed
+
+    # Getteur
+    def getXPosition(self):
+        return self.x_position
+
+    def getYPosition(self):
+        return self.y_position
 
     def getDamage(self):
         return self.damage
@@ -22,9 +33,20 @@ class Ant(object):
     def getLife(self):
         return self.life
 
+    # Setteur
+    def setXPosition(self,_x_position):
+        self.x_position = _x_position
+
+    def setYPosition(self,_y_position):
+        self.y_position = _y_position
+
+    def setDamage(self,_damage):
+        self.damage = _damage
+
+    def setFireSpeed(self, _fire_speed):
+        self.fire_speed = _fire_speed;
+
     def setLife(self, _life):
         self.life = _life
-
-
 
 
