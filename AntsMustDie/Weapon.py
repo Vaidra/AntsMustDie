022eba63151bdf115925ad833bf3c4c528295e4c
Vaidra <exaverine.27clin@yahoo.fr>
2017-06-23 18:08:37 +0200

    Weapon : Mise en place

---
+++ AntsMustDie/Weapon.py
@@ -0,0 +1,41 @@
+class Weapon(object):
+    """description of Weapon"""
+
+
+    def __init__ (
+        self, 
+        _x_position,
+        _y_position,
+        _damage     = 5, 
+        _fire_speed = 1
+    ):
+        self.x_position = _x_position
+        self.y_position = _y_position
+        self.damage     = _damage
+        self.fire_speed = _fire_speed
+
+    # Getteur
+    def getXPosition(self):
+        return self.x_position
+
+    def getYPosition(self):
+        return self.y_position
+
+    def getDamage(self):
+        return self.damage
+
+    def getFireSpeed(self):
+        return self.fire_speed
+
+    # Setteur
+    def setXPosition(self, _x_position):
+        self.x_position = _x_position
+
+    def setYPosition(self, _y_position):
+        self.y_position = _y_position
+
+    def setDamage(self, _damage):
+        self.damage = _damage
+
+    def setFireSpeed(self, _fire_speed):
+        self.fire_speed = _fire_speed
