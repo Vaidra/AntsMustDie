1546a9eb30f0f377eca365501a560df090155993
Vaidra <exaverine.27clin@yahoo.fr>
2017-06-24 12:42:19 +0200

    Weapon : modification de __init__ et création d'une fonction generate qui genère les paramètres a partir de WeaponFeature

--- AntsMustDie/Weapon.py
+++ AntsMustDie/Weapon.py
@@ -1,38 +1,46 @@
+import random
+from WeaponFeature import * 
+
 class Weapon(object):
     """description of Weapon"""
 
+    def __init__ (self):
+        self.x_position         = 0#= _x_position
+        self.y_position         = 0#= _y_position
+        self.age                = 0#= _age
+        self.damage_min         = 0#= _damage_min
+        self.damage_max         = 0#= _damage_max
+        self.damage             = 0#= _damage
+        self.fire_speed         = 0#= _fire_speed
+        self.reloading_time     = 0#= _reload_time
+        self.range              = 0#= _range
 
-    def __init__ (
-        self, 
-        _x_position,
-        _y_position,
-        _damage_min,
-        _damage_max,
-        _damage, 
-        _fire_speed_min,
-        _fire_speed_max,
-        _fire_speed,
-        _reload_time_min,
-        _reload_time_max,
-        _reload_time,
-        _range_min,
-        _range_max,
-        _range
-    ):
-        self.x_position         = _x_position
-        self.y_position         = _y_position
-        self.damage_min         = _damage_min
-        self.damage_max         = _damage_max
-        self.damage             = _damage
-        self.fire_speed_min     = _fire_speed_min
-        self.fire_speed_max     = _fire_speed_max
-        self.fire_speed         = _fire_speed
-        self.reloading_time_min = _reload_time_min
-        self.reloading_time_max = _reload_time_max
-        self.reloading_time        = _reload_time
-        self.range_min          = _range_min
-        self.range_max          = _range_max
-        self.range              = _range
+
+    #def __init__ (
+    #    self, 
+    #    _x_position,
+    #    _y_position,
+    #    _age,
+    #    _damage_min,
+    #    _damage_max,
+    #    _damage, 
+    #    _fire_speed,
+    #    _reload_time,
+    #    _range_min,
+    #    _range_max,
+    #    _range
+    #):
+    #    self.x_position         = _x_position
+    #    self.y_position         = _y_position
+    #    self.age                = _age
+    #    self.damage_min         = _damage_min
+    #    self.damage_max         = _damage_max
+    #    self.damage             = _damage
+    #    self.fire_speed         = _fire_speed
+    #    self.reloading_time     = _reload_time
+    #    self.range_min          = _range_min
+    #    self.range_max          = _range_max
+    #    self.range              = _range
 
     # Getteur
     def getXPosition(self):
@@ -50,31 +58,13 @@ class Weapon(object):
     def getDamage(self):
         return self.damage
 
-    def getFireSpeedMin(self):
-        return self.fire_speed_min
-
-    def getFireSpeedMax(self):
-        return self.fire_speed_max
-
     def getFireSpeed(self):
         return self.fire_speed
-
-    def getReloadingTimeMin(self):
-        return self.reloading_time_min
-
-    def getReloadingTimeMax(self):
-        return self.reloading_time_max
 
     def getReloadingTime(self):
         return self.reloading_time
 
-    def getRangeMin(self):
-        return self.range_min
-
-    def getRangeMax(self):
-        return self.range_max
-
-    def getRangeMin(self):
+    def getRange(self):
         return self.range
 
 
@@ -94,29 +84,82 @@ class Weapon(object):
     def setDamage(self, _damage):
         self.damage = _damage
 
-    def setFireSpeedMin(self, _fire_speed_min):
-        self.fire_speed_min = _fire_speed_min
-
-    def setFireSpeedMax(self, _fire_speed_max):
-        self.fire_speed_max = _fire_speed_max
-
     def setFireSpeed(self, _fire_speed):
         self.fire_speed = _fire_speed
-
-    def setReloadingTimeMin(self, _reload_time_min):
-        self.reloading_time_min = _reload_time_min
-
-    def setReloadingTimeMax(self, _reload_time_max):
-        self.reloading_time_max = _reload_time_max
 
     def setReloadingTime(self, _reload_time):
         self.reloading_time = _reload_time
 
-    def setRangeMin(self, _range_min):
-        self.range_min = _range_min
-    
-    def setRangeMax(self, _range_max):
-        self.range_max = _range_max
-
     def setRange(self, _range):
         self.range = _range
+
+
+    #############################
+    #############################
+    
+
+    #############################
+    # Generation
+
+    def generate(self):
+        # Tirage aléatoire pour définir l'age
+        print("nb age "+str( WeaponFeature.AGE.N_AGE))
+        self.age = random.randint(0,WeaponFeature.AGE.N_AGE)
+        print("age "+str(self.age))
+        if self.age == WeaponFeature.AGE.PREHISTORIC:
+            # Tirage aléatoire pour définir l'arme
+            weapon = random.randint(0,WeaponFeature.AGE.PREHISTORIC_WEAPON.N_PREHISTORIC_WEAPON)
+            #############################
+            if weapon == PrehistoricWeapon.BOW:
+                # Initialisation des valeurs
+                val1 = random.randint(
+                    WeaponFeature.AGE.PREHISTORIC_WEAPON.PREHISTORIC_BOW.DAMAGE_MIN, 
+                    WeaponFeature.AGE.PREHISTORIC_WEAPON.PREHISTORIC_BOW.DAMAGE_MAX
+                )
+                #tt=WeaponFeature.AGE.PREHISTORIC_WEAPON.PREHISTORIC_BOW
+                #tt.
+                val2 = random.randint(
+                    WeaponFeature.AGE.PREHISTORIC_WEAPON.PREHISTORIC_BOW.DAMAGE_MIN, 
+                    WeaponFeature.AGE.PREHISTORIC_WEAPON.PREHISTORIC_BOW.DAMAGE_MAX
+                )
+                if val1 > val2:
+                    self.damage_min = val2
+                    self.damage_max = val1
+                else:
+                    self.damage_min = val1
+                    self.damage_max = val2
+                self.fire_speed = random.randint(
+                    WeaponFeature.AGE.PREHISTORIC_WEAPON.PREHISTORIC_BOW.FIRE_SPEED_MIN,
+                    WeaponFeature.AGE.PREHISTORIC_WEAPON.PREHISTORIC_BOW.FIRE_SPEED_MAX
+                )
+                self.reloading_time = random.randint(
+                    WeaponFeature.AGE.PREHISTORIC_WEAPON.PREHISTORIC_BOW.RELOAD_TIME_MIN,
+                    WeaponFeature.AGE.PREHISTORIC_WEAPON.PREHISTORIC_BOW.RELOAD_TIME_MAX
+                )
+                self.range = random.randint(
+                    WeaponFeature.AGE.PREHISTORIC_WEAPON.PREHISTORIC_BOW.RANGE_MIN,
+                    WeaponFeature.AGE.PREHISTORIC_WEAPON.PREHISTORIC_BOW.RANGE_MAX
+                )
+                
+            #############################
+
+        ##########################################################
+        elif self.age == WeaponFeature.AGE.MIDDLE_AGES:
+            toto = 2
+
+        #self.type=rand(0,2)
+
+        #min = self.type*rangeAge
+        #min = (self.type+1)*rangeAge
+
+
+        #if TypeError==PREHIUST:
+        #    min_value = 0
+        #    max_value = 100
+
+
+        #dgt=rand(main,id,max=)
+        #self.type=preHist.type(rand))
+
+        #dgt=ArcDgt(rand(0,1))
+        #range=range(Arc (rand(0,1)))
